1e95f45630d94717962b4d28c27e2b885b1f1c56
AloKzMstf <mstf.gdik19@gmail.com>
2022-10-19 15:58:37 +0300

    Core.Security = NuGet paketleri yüklendi. Encryption, Extensions, JWT, Hashing, OtpAuthenticator, SecurityServiceRegistration eklendi.

---
+++ Core.Security/Interfaces/JWT/ITokenHelper.cs
@@ -0,0 +1,11 @@
+﻿using Core.Security.Entities;
+using Core.Security.JWT;
+
+namespace Core.Security.Interfaces.JWT
+{
+    public interface ITokenHelper
+    {
+        AccessToken CreateToken(User user, IList<OperationClaim> operationClaims);
+        RefreshToken CreateRefreshToken(User user, string ipAddress);
+    }
+}
---
+++ Core.Security/SecurityServiceRegistration.cs
@@ -0,0 +1,21 @@
+﻿using Core.Security.EmailAuthenticator;
+using Core.Security.Interfaces.EmailAuthenticator;
+using Core.Security.Interfaces.IOtpAuthenticator;
+using Core.Security.Interfaces.JWT;
+using Core.Security.JWT;
+using Core.Security.OptAuthenticator;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Core.Security
+{
+    public static class SecurityServiceRegistration
+    {
+        public static IServiceCollection AddSecurityServices(this IServiceCollection services)
+        {
+            services.AddScoped<ITokenHelper, JwtHelper>();
+            services.AddScoped<IEmailAuthenticatorHelper, EmailAuthenticatorHelper>();
+            services.AddScoped<IOtpAuthenticatorHelper, OtpAuthenticatorHelper>();
+            return services;
+        }
+    }
+}

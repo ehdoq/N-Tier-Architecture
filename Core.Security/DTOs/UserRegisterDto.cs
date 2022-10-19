6bbde58d86d86ce18831783d011fe8e5934d4a2c
AloKzMstf <mstf.gdik19@gmail.com>
2022-10-19 15:34:22 +0300

    Core.Security = Referans verildi. DTOs, Enums, Entities, EmailAuthenticator eklendi.

---
+++ Core.Security/DTOs/UserRegisterDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Core.Security.DTOs
+{
+    public class UserRegisterDto
+    {
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+    }
+}
---
+++ Core.Security/Interfaces/EmailAuthenticator/IEmailAuthenticatorHelper.cs
@@ -0,0 +1,8 @@
+﻿namespace Core.Security.Interfaces.EmailAuthenticator
+{
+    public interface IEmailAuthenticatorHelper
+    {
+        public Task<string> CreateEmailActivationKey();
+        public Task<string> CreateEmailActivationCode();
+    }
+}

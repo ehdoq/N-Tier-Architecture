3fb4432ebf95449a8b2e5ae871141cab6a8b151e
AloKzMstf <mstf.gdik19@gmail.com>
2022-10-20 13:51:36 +0300

    rentACar = Context, Repositories, PersistenceServiceRegistration eklendi.

---
+++ demoProjects/rentACar/Persistence/Context/AppDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace Persistence.Context
+{
+    public class AppDbContext : DbContext
+    {
+        protected IConfiguration? Configuration { get; set; }
+        public DbSet<Brand>? Brands { get; set; }
+
+        public AppDbContext(DbContextOptions dbContextOptions, IConfiguration configuration) : base(dbContextOptions)
+        {
+            Configuration = configuration;
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Brand>(a =>
+            {
+                a.ToTable("Brands").HasKey(k => k.Id);
+                a.Property(p => p.Id).HasColumnName("Id");
+                a.Property(p => p.Name).HasColumnName("Name");
+            });
+
+            Brand[] brandEntitySeeds = { new(1, "BMW"), new(2, "Mercedes") };
+            modelBuilder.Entity<Brand>().HasData(brandEntitySeeds);
+        }
+    }
+}
---
+++ demoProjects/rentACar/Persistence/Repositories/BrandRepository.cs
@@ -0,0 +1,15 @@
+﻿using Application.Services.Repositories;
+using Core.Persistence.Repositories;
+using Domain.Entities;
+using Persistence.Context;
+
+namespace Persistence.Repositories
+{
+    public class BrandRepository : EfRepository<Brand, AppDbContext>, IBrandRepository
+    {
+        public BrandRepository(AppDbContext context) : base(context)
+        {
+
+        }
+    }
+}

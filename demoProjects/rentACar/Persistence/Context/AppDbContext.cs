c444226002315538a27abc46de7f2933d38c3fcb
AloKzMstf <mstf.gdik19@gmail.com>
2022-10-20 16:35:43 +0300

    rentACar = Model eklendi.

--- demoProjects/rentACar/Persistence/Context/AppDbContext.cs
+++ demoProjects/rentACar/Persistence/Context/AppDbContext.cs
@@ -8,6 +8,7 @@ namespace Persistence.Context
     {
         protected IConfiguration? Configuration { get; set; }
         public DbSet<Brand>? Brands { get; set; }
+        public DbSet<Model>? Models { get; set; }
 
         public AppDbContext(DbContextOptions dbContextOptions, IConfiguration configuration) : base(dbContextOptions)
         {
@@ -21,10 +22,35 @@ namespace Persistence.Context
                 a.ToTable("Brands").HasKey(k => k.Id);
                 a.Property(p => p.Id).HasColumnName("Id");
                 a.Property(p => p.Name).HasColumnName("Name");
+                a.HasMany(p => p.Models);
             });
 
-            Brand[] brandEntitySeeds = { new(1, "BMW"), new(2, "Mercedes") };
+            modelBuilder.Entity<Model>(a =>
+            {
+                a.ToTable("Models").HasKey(k => k.Id);
+                a.Property(p => p.Id).HasColumnName("Id");
+                a.Property(p => p.BrandId).HasColumnName("BrandId");
+                a.Property(p => p.Name).HasColumnName("Name");
+                a.Property(p => p.DailyPrice).HasColumnName("DailyPrice").HasColumnType("decimal(18,2)");
+                a.Property(p => p.ImageUrl).HasColumnName("ImageUrl");
+                a.HasOne(p => p.Brand);
+            });
+
+            Brand[] brandEntitySeeds = 
+            { 
+                new(1, "BMW"), 
+                new(2, "Mercedes") 
+            };
             modelBuilder.Entity<Brand>().HasData(brandEntitySeeds);
+
+            Model[] modelEntitySeeds = 
+            {
+                new(1, 1, "Series 4", 1500m, ""), 
+                new(2, 1, "Series 3", 1200m, ""), 
+                new(3, 2, "A180", 1000m, "") ,
+                new(4, 2, "A3", 500m, "") 
+            };
+            modelBuilder.Entity<Model>().HasData(modelEntitySeeds);
         }
     }
 }

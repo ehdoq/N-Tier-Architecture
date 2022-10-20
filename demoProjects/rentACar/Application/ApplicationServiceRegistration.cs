308752f47652d0ecdba0bace90a2633c31433f8e
AloKzMstf <mstf.gdik19@gmail.com>
2022-10-20 14:37:20 +0300

    rentACar = Features\Brands\Rules eklendi.

--- demoProjects/rentACar/Application/ApplicationServiceRegistration.cs
+++ demoProjects/rentACar/Application/ApplicationServiceRegistration.cs
@@ -1,4 +1,5 @@
-﻿using MediatR;
+﻿using Application.Features.Brands.Rules;
+using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
 
@@ -11,6 +12,8 @@ namespace Application
 
             services.AddAutoMapper(Assembly.GetExecutingAssembly());
             services.AddMediatR(Assembly.GetExecutingAssembly());
+
+            services.AddScoped<BrandBusinessRules>();
 
             return services;
         }

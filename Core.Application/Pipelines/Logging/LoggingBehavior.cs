56804205a7a69bd3d192057c562364891fd036d2
AloKzMstf <mstf.gdik19@gmail.com>
2022-10-20 10:30:38 +0300

    Core.Application = Caching, Logging, Validations eklendi.

---
+++ Core.Application/Pipelines/Logging/LoggingBehavior.cs
@@ -0,0 +1,47 @@
+﻿using Core.Application.Interfaces.Logging;
+using Core.CrossCuttingConcern.Logging.Models;
+using Core.CrossCuttingConcern.Logging.Serilog;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+
+namespace Core.Application.Pipelines.Logging
+{
+    public class LoggingBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+                                                        where TRequest : IRequest<TResponse>, ILoggableRequest
+    {
+        private readonly LoggerService _loggerService;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public LoggingBehavior(LoggerService loggerService, IHttpContextAccessor httpContextAccessor)
+        {
+            _loggerService = loggerService;
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            List<LogParameter> logParameters = new()
+            {
+                new LogParameter
+                {
+                    Type = request.GetType().Name,
+                    Value = request
+                }
+            };
+
+            LogDetail logDetail = new()
+            {
+                MethodName = next.Method.Name,
+                Parameters = logParameters,
+                User = _httpContextAccessor.HttpContext == null ||
+                       _httpContextAccessor.HttpContext.User.Identity.Name == null 
+                       ? "?" : _httpContextAccessor.HttpContext.User.Identity.Name
+            };
+
+            _loggerService.Info(JsonConvert.SerializeObject(logDetail));
+
+            return next();
+        }
+    }
+}
---
+++ Core.Application/Pipelines/Validations/ValidationTool.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+
+namespace Core.Application.Pipelines.Validations
+{
+    public static class ValidationTool
+    {
+        public static void Validate(IValidator validator, object entity)
+        {
+            ValidationContext<object> context = new(entity);
+            ValidationResult result = validator.Validate(context);
+
+            if (!result.IsValid) 
+                throw new ValidationException(result.Errors);
+        }
+    }
+}

66939e43ad4b67e1f5fdbd8dba07db83e05c5bdc
AloKzMstf <mstf.gdik19@gmail.com>
2022-10-20 14:15:46 +0300

    rentACar = Features\Brands\DTOs, Commands, Mapping,  ApplicationServiceRegistration eklendi.

---
+++ demoProjects/rentACar/Application/Features/Brands/Mapping/MappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using Application.Features.Brands.Commands.CreateBrand;
+using Application.Features.Brands.DTOs;
+using AutoMapper;
+using Domain.Entities;
+
+namespace Application.Features.Brands.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Brand, CreatedBrandDto>().ReverseMap();
+            CreateMap<Brand, CreateBrandCommand>().ReverseMap();
+        }
+    }
+}
